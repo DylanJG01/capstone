8684de6abf292074d7778d5ba562a548b9f22bfa
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-04-19 10:33:07 -0700

    started story routes

---
+++ app/api/story_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Story
+
+user_routes = Blueprint('stories', __name__)
+
+
+@user_routes.route('/')
+def stories():
+    """
+    Query for all stories and returns them in a list of story dictionaries
+    """
+    stories = Story.query.all()
+    return {"stoires" : [story.to_dict() for story in stories]}
+
+@user_routes.route('/<int:id>')
+@login_required
+def story(id):
+    """
+    Query for a story by id and returns that story in a dictionary
+    """
+    user = User.query.get(id)
+    return user.to_dict()
