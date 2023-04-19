feca3040339b197a383461e610976fdce4fcac16
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-04-19 10:34:42 -0700

    fixed typo in story_routes

--- app/api/story_routes.py
+++ app/api/story_routes.py
@@ -2,10 +2,10 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Story
 
-user_routes = Blueprint('stories', __name__)
+story_routes = Blueprint('stories', __name__)
 
 
-@user_routes.route('/')
+@story_routes.route('/')
 def stories():
     """
     Query for all stories and returns them in a list of story dictionaries
@@ -13,8 +13,7 @@ def stories():
     stories = Story.query.all()
     return {"stoires" : [story.to_dict() for story in stories]}
 
-@user_routes.route('/<int:id>')
-@login_required
+@story_routes.route('/<int:id>')
 def story(id):
     """
     Query for a story by id and returns that story in a dictionary
