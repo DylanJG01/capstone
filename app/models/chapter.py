dfd1b46dac75e8e38ade31ca33df40086a586164
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-04-19 08:13:38 -0700

    attempted postgres database fix

--- app/models/chapter.py
+++ app/models/chapter.py
@@ -10,5 +10,5 @@ class Chapter(db.Model):
     body = db.Column(db.Text(), default="")
     cost = db.Column(db.Integer, default=0)
 
-    story = db.relationship('Story', backref='stories')
+    # story = db.relationship('Story', backref='stories')
     story_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('stories.id')))
--- app/models/tag_tables.py
+++ app/models/tag_tables.py
@@ -1,6 +1,5 @@
 from .db import db
 
-
 story_tags = db.Table('story_tags',
     db.Column('story_id', db.Integer, db.ForeignKey('stories.id'), primary_key=True),
     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
