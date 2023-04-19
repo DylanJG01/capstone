33d610c946ca36a29d9f68f29c8a0a2e06a5afa3
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-04-19 08:58:27 -0700

    fixed join tables

--- app/models/tag_tables.py
+++ app/models/tag_tables.py
@@ -1,11 +1,17 @@
-from .db import db, add_prefix_for_prod
+from .db import db, add_prefix_for_prod, SCHEMA, environment
 
 story_tags = db.Table('story_tags',
+    db.Model.metadata,
     db.Column('story_id', db.Integer, db.ForeignKey(add_prefix_for_prod('stories.id')), primary_key=True),
     db.Column('tag_id', db.Integer, db.ForeignKey(add_prefix_for_prod('tags.id')), primary_key=True)
 )
 
 user_tags = db.Table('user_tags',
+    db.Model.metadata,
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
     db.Column('tag_id', db.Integer, db.ForeignKey(add_prefix_for_prod('tags.id')), primary_key=True)
 )
+
+if environment == "production":
+    story_tags.schema = SCHEMA
+    user_tags.schema = SCHEMA
