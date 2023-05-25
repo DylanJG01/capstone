8e7cc544d81ff3770c64e4162d4937d6a14cf4f9
DylanJG01 <DylanJ.Godeck@gmail.com>
2023-05-24 21:20:43 -0700

    removed print statement and unhelpful comment

--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    wallet = db.Column(db.Integer())
 
     tags = db.relationship('Tag', secondary=user_tags, backref='users', lazy=True)
     # stories = db.relationship('Story', backref='users', cascade='all, delete-orphan')
